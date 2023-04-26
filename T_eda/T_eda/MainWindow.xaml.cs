bc9b3d8b51f2385bbc4177e39f1ab1c42cd5952e
teffer <55801019+teffer@users.noreply.github.com>
2023-04-26 18:29:04 +0500

    Add files via upload

---
+++ T_eda/T_eda/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace T_eda
+{
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<PortionItem> portionItems = new ObservableCollection<PortionItem>
+            {
+                new PortionItem{Num = 1, Items = "eda tut", Cost = 100},
+                new PortionItem{Num = 2, Items = "eda tut2", Cost = 300} // TODO получать с api информацию о заказах
+            };
+        public MainWindow()
+        {
+            InitializeComponent();
+            CollectionViewSource viewSource = new CollectionViewSource();
+            viewSource.Source = portionItems;
+            Portions.ItemsSource = viewSource.View;
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (Portions.SelectedItem != null)
+            {
+                portionItems.Remove((PortionItem)Portions.SelectedItem);
+            }
+        }
+
+        public class PortionItem
+        {
+            public int Num { get; set; }
+            public string Items { get; set; }
+            public decimal Cost { get; set; }
+        }
+    }
+}
